cced04348aec195640b6ea6243b24535fca225eb
Weston Kelliher <westonkelliher@gmail.com>
2023-04-12 16:37:01 -0700

    continuation of GameSate stuff for refactor

--- src/state.rs
+++ src/state.rs
@@ -11,15 +11,11 @@ enum GameState {
     //Over(OverState),
 }
 
-impl GameState {
-   //pub fn tick(
-}
-
 struct PlayingState {
     chest_states: Vec<Vec<ChestState>>,
-    //red_health_state: HealthState,
-    //blue_health_state: HealthState,
-    guess_state: GuessState,
+    red_health_state: HealthState,
+    blue_health_state: HealthState,
+    turn_state: TurnState,
 }
 
 impl PlayingState {
@@ -27,10 +23,14 @@ impl PlayingState {
         // chests
         for j in 0..ROWS {
             for i in 0..COLLUMNS {
-                
+                self.chest_states[j][i].tick();
             }
         }
         // hearts
+        self.red_health_state.tick();
+        self.blue_health_state.tick();
+        // turn
+        self.turn_state.tick();
     }
 
     fn start_open_chest(&mut self, row: usize, collumn: usize) {
@@ -43,16 +43,19 @@ impl PlayingState {
                 println!("Warning: attempt to open non-closed chest");
                 return;
             }
-            if ! self.guess_state.is_guessing() {
+            if ! self.turn_state.is_guessing() {
                 println!("Warning: attempt to open chest when guessing was not in progress");
                 return;
             }
         }
         // state change
         self.chest_states[row][collumn] = ChestState::Opening(0);
-        self.guess_state.reduce_guesses();
+        self.turn_state.reduce_guesses();
     }
 }
+
+
+pub const TICKS_CHEST_OPEN: usize = 40;
 
 #[derive(PartialEq)]
 enum ChestState {
@@ -61,23 +64,57 @@ enum ChestState {
     Open,
 }
 
+impl ChestState {
+    fn tick(&mut self) {
+        use ChestState::*;
+        if let Opening(TICKS_CHEST_OPEN) = self {
+            *self = Open;
+        } else if let Opening(n) = self {
+            *self = Opening(*n+1);
+        }
+    }
+}
 
-#[derive(PartialEq)]
-enum GuessState {
+
+pub const TICKS_TURN_TRANSITION: usize = 40;
+
+#[derive(PartialEq, Clone)]
+enum TurnState {
     RedCluing,
-    RedCluingEnd(usize/*tick number*/),
+    RedCluingEnd(usize/*tick number*/, usize/*guesses*/),
     //
     RedGuessing(usize/*guesses remaining*/),
     RedGuessingEnd(usize/*tick number*/),
     //
     BlueCluing,
-    BlueCluingEnd(usize/*tick number*/),
+    BlueCluingEnd(usize/*tick number*/, usize/*guesses*/),
     //
     BlueGuessing(usize/*guesses remaining*/),
     BlueGuessingEnd(usize/*tick number*/),
 }
 
-impl GuessState {
+impl TurnState {
+
+    fn tick(&mut self) {
+        use TurnState::*;
+        *self = match self {
+            // dynamic
+            RedCluingEnd(TICKS_TURN_TRANSITION, guesses) => RedGuessing(*guesses),
+            RedCluingEnd(n, guesses) => RedCluingEnd(*n+1, *guesses),
+            RedGuessingEnd(TICKS_TURN_TRANSITION) => BlueCluing,
+            RedGuessingEnd(n) => RedGuessingEnd(*n+1),
+            BlueCluingEnd(TICKS_TURN_TRANSITION, guesses) => BlueGuessing(*guesses),
+            BlueCluingEnd(n, guesses) => BlueCluingEnd(*n+1, *guesses),
+            BlueGuessingEnd(TICKS_TURN_TRANSITION) => RedCluing,
+            BlueGuessingEnd(n) => BlueGuessingEnd(*n+1),
+            // static
+            RedCluing => RedCluing,
+            RedGuessing(n) => RedGuessing(*n),
+            BlueCluing => BlueCluing,
+            BlueGuessing(n) => BlueGuessing(*n),
+        }
+    }
+
     fn is_guessing(&self) -> bool {
         match self {
             Self::RedGuessing(_) | Self::BlueGuessing(_) => true,
@@ -86,7 +123,7 @@ impl GuessState {
     }
 
     fn reduce_guesses(&mut self) {
-        use GuessState::*;
+        use TurnState::*;
         *self = match self {
             RedGuessing(1) => RedGuessingEnd(0),
             RedGuessing(n) => RedGuessing(*n-1),
@@ -97,10 +134,53 @@ impl GuessState {
     }
     
     pub fn current_team(&self) -> Team {
-        use GuessState::*;
+        use TurnState::*;
         match self {
-            RedCluing | RedGuessing(_) | RedCluingEnd(_) | RedGuessingEnd(_) => Team::Red,
-            BlueCluing | BlueGuessing(_) | BlueCluingEnd(_) | BlueGuessingEnd(_) => Team::Blue,            
+            RedCluing | RedGuessing(_) | RedCluingEnd(_,_) | RedGuessingEnd(_) => Team::Red,
+            BlueCluing | BlueGuessing(_) | BlueCluingEnd(_,_) | BlueGuessingEnd(_) => Team::Blue,            
         }
+    }
+}
+
+
+const TICKS_PER_HEALTH: usize = 40;
+
+struct HealthState {
+    src_amount: usize,
+    src_fraction: usize,
+    dst_amount: usize,
+}
+
+impl HealthState {
+
+    fn tick(&mut self) {
+        // decreasing if src_amount > dst_amount or src_amount == dst_amount and src_fraction > 0
+        // static once src_fraction is 0 and src_amount == dst_amount
+        // increasing if src_amount < dst_amount
+        if self.src_amount > self.dst_amount {
+            self.tick_health_down();
+        } else if self.src_amount == self.dst_amount {
+            if self.src_fraction > 0 {
+                self.tick_health_down();
+            }
+        } else if self.src_amount < self.dst_amount {
+            self.tick_health_up();
+        }
+    }
+
+    fn tick_health_up(&mut self) {
+        self.src_fraction += 1;
+        if self.src_fraction == TICKS_PER_HEALTH {
+            self.src_amount += 1;
+            self.src_fraction = 0;
+        }
+    }
+
+    fn tick_health_down(&mut self) {
+        if self.src_fraction == 0 {
+            self.src_amount -= 1;
+            self.src_fraction = TICKS_PER_HEALTH;
+        }
+        self.src_fraction -= 1;
     }
 }
