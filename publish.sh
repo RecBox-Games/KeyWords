c648f886650302288a6866d633bb099f123cbccb
requin <requin@debian.debian.requin.com>
2023-05-07 21:41:08 -0700

    no-cache for publish script

--- publish.sh
+++ publish.sh
@@ -4,3 +4,4 @@
 cp target/release/keywords ./
 tar -cfz game controller/ resources/ keywords meta.txt
 gsutil cp game gs://gamenite-games-testing/keywords/game
+gsutil setmeta -h "Cache-Control:no-cache, max-age=0" gs://gamenite-games-testing/keywords/game
