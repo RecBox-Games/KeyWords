28a72ad81dce7919d166e8fa36fe1ae6fcd711cd
Weston Kelliher <westonkelliher@gmail.com>
2023-09-26 14:35:05 -0700

    fix publish and make it check that os is buildmachine

--- publish.sh
+++ publish.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -e
+
+os_name=$(../bin/os-name.sh)
+if [[ ! "$os_name" =~ "Debian GNU/Linux 11 (bullseye)" ]]; then
+    echo "must build from a build machine"
+    echo "Exiting"
+    exit
+fi
+
 (cd ts_controller && tsc)
 cargo build --release
 cp target/release/keywords ./
-tar -cfz game controller/ resources/ keywords meta.txt
-gsutil cp game gs://gamenite-games-testing/keywords/game
+tar -czf game.tar controller/ resources/ keywords meta.txt
+gsutil cp game.tar gs://gamenite-games-testing/keywords/game
 gsutil setmeta -h "Cache-Control:no-cache, max-age=10" gs://gamenite-games-testing/keywords/game
 #gsutil setmeta -h "Cache-Control:no-store" gs://gamenite-games-testing/keywords/game
