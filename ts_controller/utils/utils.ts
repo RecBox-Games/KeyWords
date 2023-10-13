30a532ba0ca7762a2c06c7b2f87beb19f8be7ff9
Weston <westonkelliher@gmail.com>
2023-10-12 17:09:48 -0700

    change text for clue giver

--- ts_controller/utils/utils.ts
+++ ts_controller/utils/utils.ts
@@ -64,7 +64,7 @@ export const start_turn = (turn_state: TurnState) => {
     }
     else if (is_clue(board.role)) {
         if (board.role === turn_state.turn) {
-            board.topbar.text.text = 'Your turn. Give your team some keys.';
+            board.topbar.text.text = 'Give your team some keys.';
             //if(is_blue(board.role)) set_blue_input_border();
             //else set_red_input_border();
             
