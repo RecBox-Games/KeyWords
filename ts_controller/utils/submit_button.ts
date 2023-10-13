d9e24545aaa5897acf65bcde866eab41b29beb70
Safwaan Menk <41082275+safwaan-menk@users.noreply.github.com>
2023-10-13 12:09:12 -0700

    submit.png added

--- ts_controller/utils/submit_button.ts
+++ ts_controller/utils/submit_button.ts
@@ -28,14 +28,14 @@ export function init_submit_button() {
 }
 
 export function define_submit_button_properties() {    
-    submit_button.sprite.image = get_asset("buttons");
-    submit_button.sprite.src = {x: 64*4, y: 0, w: 64, h: 32};
+    submit_button.sprite.image = get_asset("submit");
+    submit_button.sprite.src = {x: 0, y: 0, w: 48, h: 32};
     const ctx = get_context();
     console.log("context dimensions x: " + ctx.dimensions.x);
 
     const base_box: Rectangle = {
-        x: ctx.dimensions.x * 0.008,
-        y: ctx.dimensions.y * 0.88,
+        x: ctx.dimensions.x * 0.0125,
+        y: ctx.dimensions.y * 0.85,
         w: ctx.dimensions.x * 0.112,
         h: ctx.dimensions.y * 0.1,
     };    
@@ -55,7 +55,7 @@ export function try_submit() {
 
 export function activate_button() {
     submit_button.is_active = true;
-    submit_button.sprite.src = {x: 64*2, y: 0, w: 64, h: 32};    
+    submit_button.sprite.src = {x: 48, y: 0, w: 48, h: 32};    
 }
 
 export function submit() {
